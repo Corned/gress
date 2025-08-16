5480501fc2d61fdcf12f6e0383a7be52a2adb63b
Oliver K <Corned@users.noreply.github.com>
2025-08-17 00:40:44 +0300

    new components for selecting heroes, max 1 tank, 2 dmg, 2 support

--- src/main.tsx
+++ src/main.tsx
@@ -5,117 +5,152 @@ import "./globals.css"
 import "./index.css"
 
 import { heroData } from "./lib/heroData"
-import HeroPicker from "./components/HeroPicker"
-import { Card } from "./components/ui/card"
 
 import { useState } from "react"
-import { IHeroDataEntry } from "./lib/heroData"
-import CompositionPicker from "./components/CompositionPicker"
+import { IHeroData } from "./lib/heroData"
 
-const Thumbnail = ({ src, alt }: { src: string; alt: string }) => {
+import CircuitRoyal from "./assets/images/maps/circuitroyal.webp"
+import { cn } from "./lib/utils"
+
+const HeroSelectorThumbnail = ({ src, alt, className = "", onClick }: { src: string; alt: string; className?: string; onClick: () => void }) => {
+  const classNames = cn(
+    "rounded-lg relative bg-white overflow-hidden border border-gray-400/50 select-none",
+    "hover:cursor-pointer hover:[&>.hero-image]:scale-110",
+    "transition-all duration-75",
+    className
+  );
   return (
-    <div className="rounded-full bg-white border-2 border-gray-200 overflow-hidden h-full">
-      <img src={src} alt={alt} className="h-full" />
+    <div
+      className={classNames}
+      onClick={onClick}
+    >
+      <img src={CircuitRoyal} alt={"background"} className="absolute h-[110%] w-[140%] z-10 object-cover blur-sm saturate-80 opacity-60" />
+      <img src={src} alt={alt} className="hero-image absolute h-full z-30 object-cover transition-transform duration-50" />
     </div>
   )
 }
 
-let selected = heroData.Dva
-const setSelected = (hero: any) => { }
 
+const HeroSelector = ({ heroes, selectedHeroes, onClickFn, limitReached }: { heroes: IHeroData[], selectedHeroes: IHeroData[], onClickFn: (hero: IHeroData) => void, limitReached: boolean }) => {
+  return (
+    <div className="grid gap-2 grid-cols-[repeat(8,1fr)] [&>*]:aspect-square">
+      {
+        heroes.map((hero) => {
+          const isSelected = selectedHeroes.some((selectedHero) => selectedHero.name === hero.name)
 
+          const classNames = cn(
+            // Every HeroSelectorThumbnail that is not selected after the limit has been reached
+            !isSelected && limitReached && "opacity-50 pointer-events-none",
+            // Selected HeroSelectorThumbnail style
+            isSelected ? "ring-2 ring-blue-400" : " ring-2 ring-transparent saturate-0"
+          );
+
+          return (
+            <HeroSelectorThumbnail
+              key={hero.name}
+              src={hero.thumbnail}
+              alt={hero.displayName}
+              className={classNames}
+              onClick={() => onClickFn(hero)}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
 
 
 
 const App = () => {
-  const [ tank1, setTank1 ] = useState<IHeroDataEntry | null>(null);
-  const [ damage1, setDamage1 ] = useState<IHeroDataEntry | null>(null);
-  const [ damage2, setDamage2 ] = useState<IHeroDataEntry | null>(null);
-  const [ support1, setSupport1 ] = useState<IHeroDataEntry | null>(null);
-  const [ support2, setSupport2 ] = useState<IHeroDataEntry | null>(null);
+  const [tanks, setTanks] = useState<IHeroData[]>([]);
+  const [damage, setDamage] = useState<IHeroData[]>([]);
+  const [supports, setSupports] = useState<IHeroData[]>([]);
+  const [compositions, setCompositions] = useState<IHeroData[][]>([]);
+
+  const selectedHeroes = [...tanks, ...damage, ...supports];
+
+  const tankHeroes = Object.values(heroData).filter(hero => hero.role === "Tank");
+  const damageHeroes = Object.values(heroData).filter(hero => hero.role === "Damage");
+  const supportHeroes = Object.values(heroData).filter(hero => hero.role === "Support");
+
+
+  // Role limits
+  const TANK_LIMIT = 1;
+  const DAMAGE_LIMIT = 2;
+  const SUPPORT_LIMIT = 2;
+
+  const IS_VALID_COMPOSITION = tanks.length === TANK_LIMIT
+    && damage.length === DAMAGE_LIMIT
+    && supports.length === SUPPORT_LIMIT;
+
+  const toggleHeroInRole = (setFn: React.Dispatch<React.SetStateAction<IHeroData[]>>, hero: IHeroData, limit: number) => {
+    setFn((prev) => {
+      if (prev.includes(hero)) {
+        return prev.filter((h) => h !== hero);
+      }
+      if (prev.length >= limit) {
+        return prev; // Do not add more than allowed
+      }
+      return [...prev, hero];
+    });
+  };
+
+
+  const handleCreateComposition = () => {
+    // Only allow if exactly 1 tank, 2 damage, 2 support
+    if (tanks.length !== TANK_LIMIT || damage.length !== DAMAGE_LIMIT || supports.length !== SUPPORT_LIMIT) return;
+    setCompositions((prev) => [[...tanks, ...damage, ...supports], ...prev]);
+    setTanks([]);
+    setDamage([]);
+    setSupports([]);
+  };
 
   return (
-    <div className="container mt-20">
-      <h1 className="text-6xl mb-10 font-bold instrument-serif text-center">scrim companion</h1>
+    <div className="container mt-10 flex flex-col gap-10 items-center">
+      <h1 className="text-[80px] font-bold instrument-serif text-center">scrim companion</h1>
 
-      <Card className="p-2">
-        <div className="flex flex-col gap-2 max-w-[400px]">
-          <HeroPicker selected={tank1} setSelected={setTank1} roleFilter="Tank"/>
-          <HeroPicker selected={damage1} setSelected={setDamage1} roleFilter="Damage"/>
-          <HeroPicker selected={damage2} setSelected={setDamage2} roleFilter="Damage"/>
-          <HeroPicker selected={support1} setSelected={setSupport1} roleFilter="Support"/>
-          <HeroPicker selected={support2} setSelected={setSupport2} roleFilter="Support"/>
-        </div>
+      <div className="flex flex-col gap-5 max-w-[700px] w-full">
 
-      </Card>
-
-      <Card className="p-2">
-        <div className="flex flex-col gap-2 max-w-[400px]">
-          <CompositionPicker selected={tank1} setSelected={setTank1} roleFilter="Tank"/>
-        </div>
-
-      </Card>
-
-
-{/*       <div className="flex flex-col gap-2">
-
-        <div className="grid grid-cols-10 items-center gap-2  bg-gray-50 rounded-lg border border-gray-200 p-2">
-          <Thumbnail src={heroData.Dva.thumbnail} alt="D.Va" />
-          <Thumbnail src={heroData.Doomfist.thumbnail} alt="Doomfist" />
-          <Thumbnail src={heroData.Hazard.thumbnail} alt="Hazard" />
-          <Thumbnail src={heroData.Junkerqueen.thumbnail} alt="Junkerqueen" />
-          <Thumbnail src={heroData.Mauga.thumbnail} alt="Mauga" />
-          <Thumbnail src={heroData.Orisa.thumbnail} alt="Orisa" />
-          <Thumbnail src={heroData.Ramattra.thumbnail} alt="Ramattra" />
-          <Thumbnail src={heroData.Reinhardt.thumbnail} alt="Reinhardt" />
-          <Thumbnail src={heroData.Roadhog.thumbnail} alt="Roadhog" />
-          <Thumbnail src={heroData.Sigma.thumbnail} alt="Sigma" />
-          <Thumbnail src={heroData.Winston.thumbnail} alt="Winston" />
-          <Thumbnail src={heroData.Wreckingball.thumbnail} alt="Wreckingball" />
-          <Thumbnail src={heroData.Zarya.thumbnail} alt="Zarya" />
-        </div>
-
-        <div className="grid grid-cols-10 items-center gap-2  bg-gray-50 rounded-lg border border-gray-200 p-2">
-          <Thumbnail src={heroData.Ashe.thumbnail} alt="Ashe" />
-          <Thumbnail src={heroData.Bastion.thumbnail} alt="Bastion" />
-          <Thumbnail src={heroData.Cassidy.thumbnail} alt="Cassidy" />
-          <Thumbnail src={heroData.Echo.thumbnail} alt="Echo" />
-          <Thumbnail src={heroData.Freja.thumbnail} alt="Freja" />
-          <Thumbnail src={heroData.Genji.thumbnail} alt="Genji" />
-          <Thumbnail src={heroData.Hanzo.thumbnail} alt="Hanzo" />
-          <Thumbnail src={heroData.Junkrat.thumbnail} alt="Junkrat" />
-          <Thumbnail src={heroData.Mei.thumbnail} alt="Mei" />
-          <Thumbnail src={heroData.Pharah.thumbnail} alt="Pharah" />
-          <Thumbnail src={heroData.Reaper.thumbnail} alt="Reaper" />
-          <Thumbnail src={heroData.Sojourn.thumbnail} alt="Sojourn" />
-          <Thumbnail src={heroData.Soldier76.thumbnail} alt="Soldier: 76" />
-          <Thumbnail src={heroData.Sombra.thumbnail} alt="Sombra" />
-          <Thumbnail src={heroData.Symmetra.thumbnail} alt="Symmetra" />
-          <Thumbnail src={heroData.Torbjorn.thumbnail} alt="Torbjörn" />
-          <Thumbnail src={heroData.Tracer.thumbnail} alt="Tracer" />
-          <Thumbnail src={heroData.Venture.thumbnail} alt="Venture" />
-          <Thumbnail src={heroData.Widowmaker.thumbnail} alt="Widowmaker" />
-        </div>
-
-        <div className="grid grid-cols-10 items-center gap-2  bg-gray-50 rounded-lg border border-gray-200 p-2">
-          <Thumbnail src={heroData.Ana.thumbnail} alt="Ana" />
-          <Thumbnail src={heroData.Baptiste.thumbnail} alt="Baptiste" />
-          <Thumbnail src={heroData.Brigitte.thumbnail} alt="Brigitte" />
-          <Thumbnail src={heroData.Illari.thumbnail} alt="Illari" />
-          <Thumbnail src={heroData.Kiriko.thumbnail} alt="Kiriko" />
-          <Thumbnail src={heroData.Lifeweaver.thumbnail} alt="Lifeweaver" />
-          <Thumbnail src={heroData.Lucio.thumbnail} alt="Lúcio" />
-          <Thumbnail src={heroData.Mercy.thumbnail} alt="Mercy" />
-          <Thumbnail src={heroData.Moira.thumbnail} alt="Moira" />
-          <Thumbnail src={heroData.Zenyatta.thumbnail} alt="Zenyatta" />
-        </div>
-
-      </div> */}
+        <HeroSelector
+          heroes={tankHeroes}
+          selectedHeroes={selectedHeroes}
+          onClickFn={(hero) => toggleHeroInRole(setTanks, hero, TANK_LIMIT)}
+          limitReached={tanks.length >= TANK_LIMIT}
+        />
+        <HeroSelector
+          heroes={damageHeroes}
+          selectedHeroes={selectedHeroes}
+          onClickFn={(hero) => toggleHeroInRole(setDamage, hero, DAMAGE_LIMIT)}
+          limitReached={damage.length >= DAMAGE_LIMIT}
+        />
+        <HeroSelector
+          heroes={supportHeroes}
+          selectedHeroes={selectedHeroes}
+          onClickFn={(hero) => toggleHeroInRole(setSupports, hero, SUPPORT_LIMIT)}
+          limitReached={supports.length >= SUPPORT_LIMIT}
+        />
 
 
+        <button
+          className={
+            cn(
+              "bg-blue-500 text-white px-6 py-2 rounded-lg font-semibold shadow transition-colors ml-auto my-5",
+              {
+                "opacity-50 cursor-not-allowed": !IS_VALID_COMPOSITION
+              }
+            )
+          }
+          onClick={handleCreateComposition}
+          disabled={!IS_VALID_COMPOSITION}
+        >
+          {IS_VALID_COMPOSITION ? "Create Composition ✅" : "Not a Valid Composition ❌"}
+        </button>
+        <a href="#step-2" className="text-blue-500 hover:underline ml-auto my-5">Next: What map did your team play?</a>
+      </div>
 
     </div>
-  )
+  );
 }
 
 
