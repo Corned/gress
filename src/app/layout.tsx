82a83360a09b1d945928c3792ce0b050028cc0e5
Oliver K <Corned@users.noreply.github.com>
2025-09-29 20:14:35 +0300

    added a title and a username button to nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,16 @@ export default function RootLayout({
       <body
         className={`antialiased p-10 min-h-screen mx-auto max-w-[1000px]`}
       >
-        <nav className="mb-20 flex gap-3 items-center justify-end h-16 rounded-lg bg-white p-2 backdrop-blur-2xl shadow">
+        <nav className="mb-20 flex gap-2 items-center h-16 rounded-lg bg-white p-2 pl-8 backdrop-blur-2xl shadow">
 
+          <p className="text-2xl font-bold">gress</p>
+
+          <div className="flex-grow"></div>
+
+
+          <div className="flex flex-row items-center rounded px-2 gap-1 h-full cursor-pointer hover:bg-gray-300 transition">
+            <p className="select-none font-semibold">Username#12345</p>
+          </div>
           <img src="/assets/icon.png" alt="icon" className="h-full aspect-square rounded" />
         </nav>
 
