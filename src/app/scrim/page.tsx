50739be6aac32c378d943eee9abfb58d99acefe3
Oliver K <Corned@users.noreply.github.com>
2025-09-29 20:00:01 +0300

    cleaned up type issues

--- src/app/scrim/page.tsx
+++ src/app/scrim/page.tsx
@@ -1,34 +1,29 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import * as htmlToImage from "html-to-image";
 
 import { mapTypeIcons, mapData, IMapData } from "@/lib/mapData";
 import { heroData, IHeroData } from "@/lib/heroData";
-
-import { BanIcon } from "lucide-react";
-
 
 const Entry = ({ map, heroBans, results, code, winningTeam }: { map: IMapData; heroBans: IHeroData[]; results: Number[]; code: string; winningTeam: Number; }) => {
 
   return (
     <div className="rounded-lg overflow-hidden relative h-16 flex flex-row gap-3 items-center p-4 z-20">
       <div className="bg-gradient-to-r from-black/60 via-black/20 to-transparent absolute inset-0 z-10 "></div>
-      <img src={map.thumbnail} alt="Map Type Icon" className="absolute object-cover w-full z-0 left-0"/>
-      <img src={mapTypeIcons[map.type]} alt="Map Type Icon" className="z-20 h-full drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)]"/>
+      <img src={map.thumbnail} alt="Map Type Icon" className="absolute object-cover w-full z-0 left-0 scale-105 blur-[1px]" />
+      <img src={mapTypeIcons[map.type]} alt="Map Type Icon" className="z-20 h-full drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)]" />
       <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)] font-bold text-xl uppercase text-center text-white z-20 ">
         {map.displayName}
       </p>
 
-
-      
       <div className="flex-grow"></div>
 
-      {/* HERO BANS */ }
+      {/* HERO BANS */}
       <div className="flex flex-row gap-2 z-20">
         {
           heroBans.map((hero) => (
-            <img key={hero.name} src={hero.thumbnail} alt={hero.name} className="h-12 z-20 relative rounded-full border-2 border-red-500 bg-gray-700 saturate-40"/>
+            <img key={hero.name} src={hero.thumbnail} alt={hero.name} className="h-12 z-20 relative rounded-full border-2 border-red-500 bg-gray-700 saturate-40" />
           ))
         }
       </div>
@@ -37,7 +32,7 @@ const Entry = ({ map, heroBans, results, code, winningTeam }: { map: IMapData; h
         // Your team wins
         winningTeam === 0 && (
           <p className="uppercase bg-green-600 z-20 relative text-white font-bold font-mono! p-2 text rounded">
-            { results.join("-") }
+            {results.join("-")}
           </p>
         )
       }
@@ -46,7 +41,7 @@ const Entry = ({ map, heroBans, results, code, winningTeam }: { map: IMapData; h
         // Enemy team wins
         winningTeam === 1 && (
           <p className="uppercase bg-red-500 z-20 relative text-white font-bold font-mono! p-2 text rounded">
-            { results.join("-") }
+            {results.join("-")}
           </p>
         )
       }
@@ -54,7 +49,7 @@ const Entry = ({ map, heroBans, results, code, winningTeam }: { map: IMapData; h
         // Draw
         winningTeam === 2 && (
           <p className="uppercase bg-gray-500 z-20 relative text-white font-bold font-mono! p-2 text rounded">
-            { results.join("-") }
+            {results.join("-")}
           </p>
         )
       }
@@ -62,7 +57,7 @@ const Entry = ({ map, heroBans, results, code, winningTeam }: { map: IMapData; h
 
 
       <p className="uppercase bg-orange-500 z-20 relative text-white font-bold font-mono! p-2 text rounded">
-        { code }
+        {code}
       </p>
     </div>
   )
@@ -71,7 +66,7 @@ const Entry = ({ map, heroBans, results, code, winningTeam }: { map: IMapData; h
 const Page = () => {
   const ref = useRef<HTMLDivElement>(null);
 
-   const handleDownload = async () => {
+  const handleDownload = async () => {
     if (ref.current === null) return;
 
     try {
@@ -87,54 +82,65 @@ const Page = () => {
 
   return (
     <div className="">
-     
+
       <div
-        ref={ref}
         className="flex flex-col gap-4"
       >
 
-        <div className="flex flex-col gap-2 w-[650px]">
+        <div
+          ref={ref}
+          className="flex flex-col gap-2 w-[650px]">
+
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-col gap-0">
+              <p className="text-3xl text-white font-black drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)]">My Team</p>
+              <p className="text-2xl text-white font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)]">vs. Enemy Team</p>
+            </div>
+            <div>
+              <p className="text-6xl text-white font-black drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)]">4.0K</p>
+            </div>
+          </div>
 
           <Entry
             map={mapData.AntarcticPeninsula}
             heroBans={[heroData.Reinhardt, heroData.Brigitte]}
-            results={ [2, 1] }
-            code={ "ABCDEF" }
+            results={[2, 1]}
+            code={"ABCDEF"}
             winningTeam={1}
           />
 
           <Entry
             map={mapData.KingsRow}
             heroBans={[heroData.Dva, heroData.Lucio]}
-            results={ [3, 3] }
-            code={ "ABCDEF" }
+            results={[3, 3]}
+            code={"ABCDEF"}
             winningTeam={2}
           />
 
           <Entry
             map={mapData.Route66}
             heroBans={[heroData.Zarya, heroData.Tracer]}
-            results={ [3, 3] }
-            code={ "ABCDEF" }
+            results={[3, 3]}
+            code={"ABCDEF"}
             winningTeam={1}
           />
 
           <Entry
             map={mapData.Runasapi}
             heroBans={[heroData.WreckingBall, heroData.Wuyang]}
-            results={ [1, 0] }
-            code={ "ABCDEF" }
+            results={[1, 0]}
+            code={"ABCDEF"}
             winningTeam={0}
           />
 
           <Entry
             map={mapData.NewJunkCity}
             heroBans={[heroData.Kiriko, heroData.Sigma]}
-            results={ [2, 3] }
-            code={ "ABCDEF" }
+            results={[2, 3]}
+            code={"ABCDEF"}
             winningTeam={1}
           />
-          
+
 
         </div>
 
