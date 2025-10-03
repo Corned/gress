c9b4cf0f56e7e0fb3d8b1727613bd58e77584a5e
Oliver K <Corned@users.noreply.github.com>
2025-10-03 23:56:40 +0300

    migrating from nextjs to nuxt, vercel's ceo is weird

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
-
-export default eslintConfig;
+export default withNuxt(
+  // Your custom configs here
+)
