02017ef74006e01fdfe37adab7029b98d56b857c
Oliver K <Corned@users.noreply.github.com>
2026-01-18 16:30:04 +0200

    server port is now in .env

--- server/server.go
+++ server/server.go
@@ -1,28 +1,32 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+   
+    if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file")
+    }
+    
     r := gin.Default()
 
-    // 1. Serve static files (CSS, JS, Images)
-    // specific folders from inside your 'public' directory
     r.Static("/_nuxt", "./public/_nuxt")
-    r.Static("/assets", "./public/assets") // if you have one
+    r.Static("/assets", "./public/assets")
     r.StaticFile("/favicon.ico", "./public/favicon.ico")
 
-    // 2. Serve the index.html for the root route
     r.GET("/", func(c *gin.Context) {
         c.File("./public/index.html")
     })
 
-    // 3. SPA Fallback (The important part!)
-    // If a route is not found (like /dashboard), serve index.html
     r.NoRoute(func(c *gin.Context) {
         c.File("./public/index.html")
     })
 
-    r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":" + os.Getenv("PORT"))
 }
