9813698476c76db4dd0195e1f1388df24dfc8761
Corned <Corned@users.noreply.github.com>
2026-02-18 14:29:55 +0200

    recentmatch component

---
+++ client/app/components/RecentMatch.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { heroData } from "~/lib/heroData";
+import { mapData } from "~/lib/mapData";
+
+type MapKey = keyof typeof mapData;
+
+interface Match {
+  map: MapKey;
+  team1Ban: string;
+  team2Ban: string;
+  team1Score: number;
+  team2Score: number;
+}
+
+const testData: Match[] = [
+  {
+    map: "Ilios",
+    team1Ban: "Widowmaker",
+    team2Ban: "Tracer",
+    team1Score: 0,
+    team2Score: 3,
+  },
+  {
+    map: "KingsRow",
+    team1Ban: "Bastion",
+    team2Ban: "Reaper",
+    team1Score: 3,
+    team2Score: 2,
+  },
+  {
+    map: "Dorado",
+    team1Ban: "Ana",
+    team2Ban: "Genji",
+    team1Score: 1,
+    team2Score: 3,
+  },
+  {
+    map: "Runasapi",
+    team1Ban: "Reinhardt",
+    team2Ban: "Zarya",
+    team1Score: 2,
+    team2Score: 1,
+  },
+  {
+    map: "NewJunkCity",
+    team1Ban: "Cassidy",
+    team2Ban: "Ashe",
+    team1Score: 3,
+    team2Score: 2,
+  },
+];
+</script>
+
+<template>
+  <div class="p-2 bg-gray-100 rounded-lg w-fit flex flex-col gap-[4px]">
+
+    <p class="text-lg font-bold">MRG Garnet vs xxx</p>
+
+    <div v-for="(match, index) in testData" :key="index"
+      class="grid grid-cols-[auto_1fr_auto] gap-3 items-center rounded-sm bg-white p-2 pr-4" :class="{
+        'rounded-t-lg': index === 0,
+        'rounded-b-lg': index === testData.length - 1,
+      }">
+      <img :src="mapData[match.map].thumbnail" alt="map" class="aspect-[2/1] object-cover h-8 rounded" />
+      <p class="text-lg font-semibold">{{ mapData[match.map].displayName }}</p>
+      <p class="text-lg font-semibold ml-4">{{ match.team1Score }} - {{ match.team2Score }}</p>
+    </div>
+
+  </div>
+</template>
--- client/app/pages/dashboard.vue
+++ client/app/pages/dashboard.vue
@@ -1,17 +1,5 @@
 <script setup lang="ts">
-import { mapData } from '~/lib/mapData';
-import { heroData } from '~/lib/heroData';
 
-
-const entryData = {
-  map: mapData['KingsRow'],
-  team1Ban: heroData.Lucio,
-  team2Ban: heroData.Dva,
-  team1Score: 0,
-  team2Score: 0,
-  code: '',
-  winningTeam: 0 // 0: Us, 1: Enemy, 2: Draw
-}
 </script>
 
 <template>
@@ -26,31 +14,29 @@ const entryData = {
         <img src="/assets/icon.png" alt="icon" class="h-full rounded" />
       </div>
     </nav>
-    <aside class="col-span-1 flex flex-col px-4">
 
-      <input class="w-full rounded-md bg-gray-200 px-3 py-1 mb-3" placeholder="filter" />
+    <aside class="col-span-1 flex flex-col gap-3 px-4">
+      <button
+        class="w-full h-12 rounded-md text-lg bg-gray-200 px-6 font-bold cursor-pointer hover:bg-gray-300 transition-colors duration-100">Dashboard</button>
+
+      <!--<input class="w-full rounded-md bg-gray-200 px-3 py-1" placeholder="filter" />-->
       <TreeNav />
 
       <footer class="w-full mt-auto flex justify-center items-center p-2">
         <p class="text-center text-sm font-semibold text-gray-400">made with spite and hatred</p>
       </footer>
     </aside>
+
     <main class="col-span-1 row-span-2 bg-white rounded-tl-xl px-6 pt-10">
-      <h1 class="text-3xl font-bold mb-6">Dashboard</h1>
+      <h1 class="text-3xl font-bold mb-4">Dashboard</h1>
+      <h2 class="text-2xl font-semibold mb-4">Your recent matches</h2>
 
-      <div class="flex flex-col gap-1">
+      <div class="flex flex-row gap-2">
 
-        <ScrimEntry :map="entryData.map" :team1Ban="entryData.team1Ban" :team2Ban="entryData.team2Ban"
-          :team1Score="entryData.team1Score" :team2Score="entryData.team2Score" :code="entryData.code"
-          :winningTeam="entryData.winningTeam" class="max-w-[660px]" />
 
-        <ScrimEntry :map="entryData.map" :team1Ban="entryData.team1Ban" :team2Ban="entryData.team2Ban"
-          :team1Score="entryData.team1Score" :team2Score="entryData.team2Score" :code="entryData.code"
-          :winningTeam="entryData.winningTeam" class="max-w-[660px]" />
+        <RecentMatch />
+        <RecentMatch />
 
-        <ScrimEntry :map="entryData.map" :team1Ban="entryData.team1Ban" :team2Ban="entryData.team2Ban"
-          :team1Score="entryData.team1Score" :team2Score="entryData.team2Score" :code="entryData.code"
-          :winningTeam="entryData.winningTeam" class="max-w-[660px]" />
       </div>
     </main>
   </section>
