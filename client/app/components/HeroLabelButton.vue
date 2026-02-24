712189777def7fa6b5d529c9e66bc3c18a5b2f55
Corned <Corned@users.noreply.github.com>
2026-02-24 12:43:19 +0200

    added white background

--- client/app/components/HeroLabelButton.vue
+++ client/app/components/HeroLabelButton.vue
@@ -10,7 +10,7 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <div class="flex flex-row gap-2 border-2 rounded-full pl-1 pr-3 py-1 items-center border-zinc-200 select-none
+  <div class="flex flex-row gap-2 border-2 rounded-full pl-1 pr-3 py-1 items-center border-zinc-200 select-none bg-white
            transition-all duration-200 ease-in-out
            hover:cursor-pointer hover:-translate-y-0.5 hover:shadow-md hover:border-zinc-300">
     <img :src="heroData[props.hero].thumbnail" alt="" class="h-full rounded-full bg-zinc-200">
