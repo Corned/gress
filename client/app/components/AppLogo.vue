dec4bb46b8c3060b5397cfefc91ff2048d420c7b
Oliver K <Corned@users.noreply.github.com>
2026-03-14 21:11:14 +0200

    applogo component

---
+++ client/app/components/AppLogo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  size?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  size: 'text-3xl'
+})
+</script>
+
+<template>
+  <div class="flex items-baseline">
+    <span :class="[size, 'font-bold text-zinc-300']">pro</span>
+    <span :class="[size, 'font-bold text-zinc-950']">gress</span>
+  </div>
+</template>
