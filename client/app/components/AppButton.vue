d82333c051c99db98d3c531210dbab6bb5bbf387
Oliver K <Corned@users.noreply.github.com>
2026-03-14 21:19:58 +0200

    separated navigation from the button

--- client/app/components/AppButton.vue
+++ client/app/components/AppButton.vue
@@ -3,20 +3,18 @@ interface Props {
   label: string
   bg?: string
   textColor?: string
-  to?: string
 }
 
-const props = withDefaults(defineProps<Props>(), {
-  bg: 'bg-zinc-200/75',
+withDefaults(defineProps<Props>(), {
+  bg: 'bg-zinc-200',
   textColor: 'text-zinc-900',
 })
-
-const tag = computed(() => props.to ? resolveComponent('NuxtLink') : 'button')
 </script>
 
 <template>
-  <component :is="tag" :to="props.to" :class="[bg, textColor]" class="w-full h-12 rounded-lg px-6 font-bold cursor-pointer select-none
+  <button :class="[bg, textColor]"
+    class="w-full h-12 rounded-lg px-6 font-bold cursor-pointer select-none
            transition-all duration-150 hover:brightness-95 hover:-translate-y-px grid place-items-center">
     <span>{{ label }}</span>
-  </component>
+  </button>
 </template>
---
+++ client/app/components/NavButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({ inheritAttrs: false })
+
+defineProps<{ to: string; label: string }>()
+</script>
+
+<template>
+  <NuxtLink :to="to">
+    <AppButton v-bind="$attrs" :label="label" />
+  </NuxtLink>
+</template>
