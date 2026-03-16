dabba79459706836cc5b56b144077046976d9b71
Oliver K <Corned@users.noreply.github.com>
2026-03-16 18:17:09 +0200

    buttons are actual buttons

--- client/app/components/HeroLabelButton.vue
+++ client/app/components/HeroLabelButton.vue
@@ -10,10 +10,10 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <div class="flex flex-row gap-2 border rounded-full pl-1 pr-3 py-1 items-center border-zinc-300 select-none bg-white
+  <button class="flex flex-row gap-2 border rounded-full pl-1 pr-3 py-1 items-center border-zinc-300 select-none bg-white
            transition-all duration-200 ease-in-out
            hover:cursor-pointer hover:-translate-y-0.5 hover:border-zinc-400">
     <img :src="heroData[props.hero].thumbnail" alt="" class="h-full rounded-full bg-zinc-300">
     <p class="font-semibold">{{ label }}</p>
-  </div>
+  </button>
 </template>
--- client/app/components/IconLabelButton.vue
+++ client/app/components/IconLabelButton.vue
@@ -10,10 +10,10 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <div class="flex flex-row gap-2 border rounded-md bg-white pl-3 pr-3 items-center border-zinc-300 select-none h-10
+  <button class="flex flex-row gap-2 border rounded-md bg-white pl-3 pr-3 items-center border-zinc-300 select-none h-10
            transition-all duration-200 ease-in-out
            hover:cursor-pointer hover:-translate-y-0.5 hover:border-zinc-400">
     <component :is="icon" stroke-width="2" class="size-4 shrink-0" />
     <p v-if="label" class="font-semibold text-sm">{{ label }}</p>
-  </div>
+  </button>
 </template>
