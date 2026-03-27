a04b17353e835f4c2852a84d4381d7a7f4579b29
Oliver K <Corned@users.noreply.github.com>
2026-03-28 00:09:55 +0200

    added max height

--- client/app/components/HeroLabelButton.vue
+++ client/app/components/HeroLabelButton.vue
@@ -10,8 +10,7 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <button class="btn-base flex flex-row gap-2 border rounded-full pl-1 pr-3 py-1 items-center border-zinc-200 select-none bg-white
-           hover:border-zinc-300">
+  <button class="btn-base flex flex-row gap-2 border rounded-full pl-1 pr-3 py-1 items-center border-zinc-200 select-none bg-white hover:border-zinc-300 whitespace-nowrap h-10 max-h-10">
     <img :src="heroData[props.hero].thumbnail" alt="" class="h-full rounded-full bg-zinc-300">
     <p class="font-semibold">{{ label }}</p>
   </button>
